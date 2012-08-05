fd70f53b7bd6544b502e673b8d1402ce569f5979
Matthew Harvey <software@matthewharvey.net>
2012-08-05 05:17:55 +0000

    Added documentation to exception_macros.hpp

--- exception_macros.hpp
+++ exception_macros.hpp
@@ -2,6 +2,21 @@
 #define GUARD_exception_macros_hpp
 
 
+/** @file exception_macros.hpp
+ *
+ * @brief Provides macros to ease the declaration and definition of
+ * exception classes.
+ *
+ * The boilerplate involved in creating exception
+ * classes can discourage their creation. While macros are generally to
+ * be avoided, having a reasonably fine-grained structure of exceptions
+ * is a good thing - which unfortunately is discouraged by the amount
+ * of boilerplate involved in creating such a structure. These macros
+ * aim to vastly reduce the repetitive boilerplate required to create
+ * a hierarchy of exception classes.
+ *
+ */
+
 
 /**
  * Macro to declare and define an exception class named \c CLASS, inheriting
@@ -13,8 +28,8 @@
  * std::string and std::exception are available (typically in a location that
  * has included <string> and <stdexcept>).
  *
- * Note the mecro definition omits a trailing semi-colon. A semi-colon should
- * should be included when the macro is invoked.
+ * Note the macro definition omits a trailing semi-colon. A semi-colon should
+ * always be appended when the macro is invoked.
  */
 #define JEWEL_STANDARD_EXCEPTION(CLASS)\
 	class CLASS: public std::exception\
@@ -45,8 +60,8 @@
  * std::string and std::exception are available (typically in a location that
  * has included <string> and <stdexcept>).
  *
- * Note the mecro definition omits a trailing semi-colon. A semi-colon should
- * should be included when the macro is invoked.
+ * Note the macro definition omits a trailing semi-colon. A semi-colon should
+ * always be appended when the macro is invoked.
  */
 #define JEWEL_DERIVED_EXCEPTION(DERIVED_CLASS, BASE_CLASS)\
 	class DERIVED_CLASS: public BASE_CLASS\
