7d5d7aaa716923cbd0ce1a1bf7a5ca934d7f237f
Matthew Harvey <software@matthewharvey.net>
2013-03-24 16:26:34 +0000

    Created jewel::Signature<...> class template to provide for "class authentication objects", to facilitate function-level access control.

---
+++ signature.hpp
@@ -0,0 +1,44 @@
+#ifndef GUARD_signature_hpp
+#define GUARD_signature_hpp
+
+#include <boost/noncopyable.hpp>
+
+
+namespace jewel
+{
+
+
+/**
+ * The instantiation of this template for a class T is a
+ * non-copyable class the constructor of which is only
+ * accessible by
+ * class T. This provides a mechanism whereby a
+ * function that wants to ensure at compile time
+ * that it can only be called by class T, can achieve
+ * this simply by having a
+ * parameter of type Signature<T>& (or Signature<T> const&).
+ * This provides a fine-grained, function-level means of
+ * access control, that is not directly provided by
+ * the \e friend mechanism.
+ */
+template <class T>
+class Signature:
+	private boost::noncopyable
+{
+public:
+	friend T;
+private:
+	Signature();
+};
+
+
+template <class T>
+inline
+Signature<T>::Signature()
+{
+}
+
+
+}  // namespace jewel
+
+#endif  // GUARD_signature_hpp
