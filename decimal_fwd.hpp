6ef0666eb106108a2f50b75c8a4d233569b5d33b
Matthew Harvey <software@matthewharvey.net>
2013-06-04 10:27:35 +0000

    Created decimal_fwd.hpp to facilitate forward declaration of jewel::Decimal in client code.

---
+++ decimal_fwd.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2013, Matthew Harvey. All rights reserved.
+
+#ifndef GUARD_decimal_fwd_hpp
+#define GUARD_decimal_fwd_hpp
+
+// Contains forward declaration of Decimal class.
+
+namespace jewel
+{
+
+class Decimal;
+
+}  // namespace jewel
+
+#endif  // GUARD_decimal_fwd_hpp
