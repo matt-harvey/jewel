34b1ad946a93e21faed7013032473ebf92c9d8df
Matthew Harvey <software@matthewharvey.net>
2013-08-30 13:39:42 +0000

    Created JEWEL_DEBUG_LOG_VALUE(x) macro to facilitate logging an expression, and its value, to std::clog when debugging.

--- debug_log.hpp
+++ debug_log.hpp
@@ -13,16 +13,21 @@
 #include <iostream>
 
 
-/**Macros to print to std::clog iff DEBUG is defined.
+/**Macros to print to std::clog if DEBUG is defined, or else to compile
+ * away to nothing if DEBUG is not defined.
  *
  * If DEBUG is defined, JEWEL_DEBUG_LOG acts like std::clog
- * (but prints "  LOG: " before the rest of the output),
+ * (but prints "LOG: " before the rest of the output),
  * but if DEBUG is not defined, JEWEL_DEBUG_LOG, and the rest
  * of the line, is effectively compiled away to nothing.
  *
  * If DEBUG is defined JEWEL_DEBUG_LOG_LOCATION prints the
  * file and line number to std::clog; otherwise, it is
  * effectively compiled away to nothing.
+ *
+ * If DEBUG is defined JEWEL_DEBUG_LOG_VALUE(x) prints a string
+ * of the form "LOG: x: [value of x]". E.g. if x == 3, then
+ * JEWEL_DEBUG_LOG_VALUE(x) will print "LOG: x: 3".
  *
  * Note a semicolon must still be included by client code
  * at the end of a statement using either of these macros.
@@ -37,11 +42,13 @@
  */ 
 
 #ifdef DEBUG
-	#define JEWEL_DEBUG_LOG std::clog << "  LOG: "
+	#define JEWEL_DEBUG_LOG std::clog << "LOG: "
 	#define JEWEL_DEBUG_LOG_LOCATION std::clog << __FILE__ << ": " << __LINE__ << std::endl;
+	#define JEWEL_DEBUG_LOG_VALUE(x) std::clog << "LOG: " #x ": " << x << std::endl;
 #else
 	#define JEWEL_DEBUG_LOG 0 && std::clog
 	#define JEWEL_DEBUG_LOG_LOCATION 0
+	#define JEWEL_DEBUG_LOG_VALUE(X) 0
 #endif
 
 
