c85681300ed5546bb69b049d2f8931e8ba3b686e
Matthew Harvey <software@matthewharvey.net>
2013-09-05 19:21:00 +0000

    Created jewel::TolerantString class. This is similar in intent to CappedString<N>, in that it's a string class the member functions of which cannot throw exceptions. Unlike CappedString, it is dynamically sized. Unlike std::string, if there is memory allocation failure internally, instead of throwing std::bad_alloc, it marks itself as invalid (a state which can be queried by the user), and becomes otherwise equivalent to an empty string. I still need to write unit tests for jewel::TolerantString.

---
+++ tolerant_string.cpp
@@ -0,0 +1,128 @@
+#include "tolerant_string.hpp"
+#include "assert.hpp"
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+
+using std::equal;
+using std::free;
+using std::malloc;
+using std::strcpy;
+using std::strlen;
+
+namespace jewel
+{
+
+TolerantString::TolerantString():
+	m_is_valid(true),
+	m_len(0),
+	m_data(0),
+	m_standby("")
+{
+}
+
+TolerantString::TolerantString(char const* p_string):
+	m_is_valid(true),
+	m_len(0),
+	m_data(0),
+	m_standby("")
+{
+	initialize_from_c_string(p_string);
+}
+
+TolerantString::TolerantString(TolerantString const& rhs):
+	m_is_valid(true),
+	m_len(0),
+	m_data(0),
+	m_standby("")
+{
+	initialize_from_c_string(rhs.c_str());
+}
+
+TolerantString::~TolerantString()
+{
+	free(m_data);
+	m_data = 0;
+}
+
+TolerantString&
+TolerantString::operator=(TolerantString const& rhs)
+{
+	if (this != &rhs)
+	{
+		initialize_from_c_string(rhs.c_str());
+	}
+	return *this;
+}
+
+bool
+TolerantString::operator==(TolerantString const& rhs) const
+{
+	return
+	(	(is_valid() == rhs.is_valid()) &&
+		(size() == rhs.size()) &&
+		equal(m_data, m_data + m_len, rhs.m_data)
+	);
+}
+
+char const*
+TolerantString::c_str() const
+{
+	return m_data? m_data: m_standby.c_str();
+}
+
+TolerantString::size_type
+TolerantString::size() const
+{
+	return m_len;
+}
+
+bool
+TolerantString::empty() const
+{
+	return m_len == 0;
+}
+
+bool
+TolerantString::is_valid() const
+{
+	return m_is_valid;
+}
+
+void
+TolerantString::clear()
+{
+	free(m_data);
+	m_data = 0;
+	m_is_valid = true;
+	m_len = 0;
+	JEWEL_ASSERT(m_standby == CappedString<1>());
+	return;
+}
+
+void
+TolerantString::swap(TolerantString& rhs)
+{
+	using std::swap;
+	swap(m_is_valid, rhs.m_is_valid);
+	swap(m_len, rhs.m_len);
+	swap(m_data, rhs.m_data);
+	JEWEL_ASSERT(m_standby == rhs.m_standby);
+	return;
+}
+
+void
+TolerantString::initialize_from_c_string(char const* p_string)
+{
+	m_len = strlen(p_string);
+	m_data = static_cast<char*>(malloc(sizeof(char) * (m_len + 1)));
+	m_is_valid = (m_data != 0);
+	strcpy(m_data, p_string);
+	JEWEL_ASSERT(m_standby == CappedString<1>());
+	return;
+}
+
+
+
+}  // namespace jewel
---
+++ tolerant_string.hpp
@@ -0,0 +1,87 @@
+#ifndef GUARD_tolerant_string_hpp
+#define GUARD_tolerant_string_hpp
+
+#include "capped_string.hpp"
+#include <cstddef>
+#include <cstdlib>
+
+namespace jewel
+{
+
+/**
+ * Represents a dynamically allocated string of char, which is similar to
+ * std::string except that:\n
+ * (a) It lacks many of the functions and conveniences of std::string; and\n
+ * (b) Its member functions never throw exceptions. In particular,
+ * \e std::bad_alloc is never thrown. Rather, if there is a failure of
+ * memory allocation during construction, assignment or etc.., the
+ * TolerantString is marked as invalid, and its contents become equivalent
+ * to an empty string. The validity of a TolerantString can
+ * be queried via the \e is_valid() member function. This is useful in
+ * cases where we want a string class that cannot possibly throw, for
+ * example as the member variable of an exception class representing
+ * an error message.
+ *
+ * @todo HIGH PRIORITY Testing.
+ */
+class TolerantString
+{
+public:
+	typedef char const* const_iterator;
+	typedef char* iterator;
+	typedef std::size_t size_type;
+	typedef std::ptrdiff_t difference_type;
+	typedef char value_type;
+
+	TolerantString();
+	TolerantString(char const* p_string);
+	TolerantString(TolerantString const& rhs);
+	~TolerantString();
+	TolerantString& operator=(TolerantString const& rhs);
+
+	/**
+	 * Two TolerantString compare as equal if and only if they have the
+	 * same length with the same characters in the same order. If neither
+	 * string is valid, then they will always compare equal. If one string is
+	 * valid and the other is not, then they will never compare as equal,
+	 * even if their contents are the same (the contents of an invalid
+	 * string are always the empty string).
+	 */
+	bool operator==(TolerantString const& rhs) const;
+	bool operator!=(TolerantString const& rhs) const;
+
+	/**
+	 * Similar to std::string::c_str(), with similar caveats. This
+	 * returns an empty string if the TolerantString is invalid.
+	 */
+	char const* c_str() const;
+
+	size_type size() const;
+
+	bool empty() const;
+
+	/**
+	 * TolerantString may undergo memory allocation failure internally.
+	 * If this happens, then rather than std::bad_alloc being thrown,
+	 * the TolerantString simply marked as invalid. The validity of
+	 * the TolerantString can be tested with the is_valid() member function.
+	 */
+	bool is_valid() const;
+
+	void clear();
+
+	void swap(TolerantString& rhs);
+
+private:
+	void initialize_from_c_string(char const* p_string);
+
+	bool m_is_valid;
+	size_type m_len;
+	char* m_data;
+	CappedString<1> m_standby;
+
+};  // class TolerantString
+
+}  // namespace jewel
+
+#endif  // GUARD_tolerant_string_hpp
