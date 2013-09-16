d82e66c6fd7dfd96495ddbc9382f5ab06d9b37ac
Matthew Harvey <software@matthewharvey.net>
2013-09-16 11:45:03 +0000

    Made it so that, in "windows.hpp", if we're on Windows, then JEWEL_ON_WINDOWS will BE DEFINED, but not be defined as 1 or as any other particular value. This avoids confusion as to whether client needs to test the value of the macro. It is now clear the they only need to test whether it is defined or not.

--- on_windows.hpp
+++ on_windows.hpp
@@ -7,12 +7,12 @@
 #	if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
 
 		/**
-		 * Single macro that is defined as 1 if and only if we are
+		 * Single macro that is defined if and only if we are
 		 * on Windows, otherwise is not defined at all. This avoids having
 		 * to type multiple underscore-related variations of "WIN32" every
 		 * time we want to test whether we are on Windows.
 		 */
-#		define JEWEL_ON_WINDOWS 1
+#		define JEWEL_ON_WINDOWS
 
 #	endif
 
