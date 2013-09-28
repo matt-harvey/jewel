25cd52f9ee3c783d4982b2b6cb5f834917817cc4
Matthew Harvey <software@matthewharvey.net>
2013-09-28 12:49:34 +0000

    Provided forward declaration headers for CappedString<...> and TolerantString.

---
+++ capped_string_fwd.hpp
@@ -0,0 +1,14 @@
+#ifndef GUARD_capped_string_fwd_hpp_6931946215653767
+#define GUARD_capped_string_fwd_hpp_6931946215653767
+
+#include <cstddef>
+
+namespace jewel
+{
+
+template <std::size_t N> class CappedString;
+
+
+}  // namespace jewel
+
+#endif  // GUARD_capped_string_fwd_hpp_6931946215653767
---
+++ tolerant_string_fwd.hpp
@@ -0,0 +1,11 @@
+#ifndef GUARD_tolerant_string_fwd_hpp_49371402268005254
+#define GUARD_tolerant_string_fwd_hpp_49371402268005254
+
+namespace jewel
+{
+
+class TolerantString;
+
+}  // namespace jewel
+
+#endif  // GUARD_tolerant_string_fwd_hpp_49371402268005254
