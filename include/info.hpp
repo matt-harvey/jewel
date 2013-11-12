4838b42c26535c31687b07fd235f84e6cd498fbf
Matthew Harvey <software@matthewharvey.net>
2013-11-12 03:15:22 +0000

    Created jewel::Info class, to provide information about the library versioning etc.. Currently has a "version()" method only.
    Created forward declaration header for Version class.

---
+++ include/info.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012-2013 Matthew Harvey
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GUARD_info_hpp_6414792601212297
+#define GUARD_info_hpp_6414792601212297
+
+#include "version_fwd.hpp"
+
+namespace jewel
+{
+
+/**
+ * Provides information about the Jewel library.
+ */
+class Info
+{
+public:
+
+	/**
+	 * @returns a Version object representing the library version.
+	 *
+	 * Exception safety: <em>nothrow guarantee</em>.
+	 */
+	static Version version();
+
+	Info() = delete;
+	Info(Info const& rhs) = delete;
+	Info(Info&& rhs) = delete;
+	Info& operator=(Info const& rhs) = delete;
+	Info& operator=(Info&& rhs) = delete;
+	virtual ~Info() = delete;
+
+};  // class Info
+
+}  // namespace jewel
+
+#endif  // GUARD_info_hpp_6414792601212297
